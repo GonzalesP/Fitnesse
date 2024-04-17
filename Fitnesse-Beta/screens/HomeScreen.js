31102971ae3b5aea6f6b754026dcdc437ee6a75f
P Gonz <GonzalesP@csu.fullerton.edu>
2024-04-17 09:34:38 -0700

    Added navigation from the Home Screen to the Record Today's Workout and Record Today's Weight screens.

--- Fitnesse-Beta/screens/HomeScreen.js
+++ Fitnesse-Beta/screens/HomeScreen.js
@@ -1,25 +1,43 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 
-const HomeScreen = () => {
+export default function HomeScreen({ navigation }) {
+  function debugStuff() {
+    console.log("hi")
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello</Text>
+      <Pressable onPress={() => navigation.navigate('Profile Stack', { screen: 'Record Weight' })}>
+        <Text style={styles.testText}>Go to 'Record Today's Weight' screen</Text>
+      </Pressable>
+      <Pressable onPress={() => navigation.navigate('Workout Stack', { screen: 'Record Workout' })}>
+        <Text style={styles.testText}>Go to 'Record Today's Workout' screen</Text>
+      </Pressable>
+      <Pressable onPress={debugStuff}>
+        <Text style={styles.debug}>Scuffed Debug Button</Text>
+      </Pressable>
     </View>
   );
 }
-
-export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    // justifyContent: "center",
     // backgroundColor: "#225588",
   },
   text: {
     fontSize: 24,
     fontWeight: "bold",
+    marginBottom: 16,
+    // color: "#F4F5F5"
+  },
+  debug: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
     marginBottom: 16,
     // color: "#F4F5F5"
   }
