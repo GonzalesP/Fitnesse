e0ae67a8bcde848d2b78a10a07579967e08a5cb9
P Gonz <GonzalesP@csu.fullerton.edu>
2024-04-19 10:54:58 -0700

    Created debug data sets to use while constructing UI components

---
+++ Fitnesse-Beta/data/debugDataSets.js
@@ -0,0 +1,870 @@
+export const debugHeight =
+{
+  "feet": 5,
+  "inches": 8
+}
+
+export const debugWeightHistory =
+[
+  {
+    "date": "Sun Mar 17 2024",
+    "weight": 334.6
+  },
+  {
+    "date": "Mon Mar 18 2024",
+    "weight": 333.6
+  },
+  {
+    "date": "Tue Mar 19 2024",
+    "weight": 332.6
+  },
+  {
+    "date": "Wed Mar 20 2024",
+    "weight": 331.6
+  },
+  {
+    "date": "Thu Mar 21 2024",
+    "weight": 328.8
+  },
+  {
+    "date": "Fri Mar 22 2024",
+    "weight": 329.8
+  },
+  {
+    "date": "Sat Mar 23 2024",
+    "weight": 329.8
+  },
+  {
+    "date": "Sun Mar 24 2024",
+    "weight": 330.6
+  },
+  {
+    "date": "Mon Mar 25 2024",
+    "weight": 330.1
+  },
+  {
+    "date": "Tue Mar 26 2024",
+    "weight": 329.6
+  },
+  {
+    "date": "Wed Mar 27 2024",
+    "weight": 326.0
+  },
+  {
+    "date": "Thu Mar 28 2024",
+    "weight": 325.6
+  },
+  {
+    "date": "Fri Mar 29 2024",
+    "weight": 325.4
+  },
+  {
+    "date": "Sat Mar 30 2024",
+    "weight": 322.8
+  },
+  {
+    "date": "Sun Mar 31 2024",
+    "weight": 322.0
+  },
+  {
+    "date": "Mon Apr 01 2024",
+    "weight": 324.2
+  },
+  {
+    "date": "Tue Apr 02 2024",
+    "weight": 322.0
+  },
+  {
+    "date": "Wed Apr 03 2024",
+    "weight": 324.0
+  },
+  {
+    "date": "Thu Apr 04 2024",
+    "weight": 325.6
+  },
+  {
+    "date": "Fri Apr 05 2024",
+    "weight": 324.6
+  },
+  {
+    "date": "Sat Apr 06 2024",
+    "weight": 323.4
+  },
+  {
+    "date": "Sun Apr 07 2024",
+    "weight": 323.6
+  },
+  {
+    "date": "Mon Apr 08 2024",
+    "weight": 323.2
+  },
+  {
+    "date": "Tue Apr 09 2024",
+    "weight": 324.0
+  },
+  {
+    "date": "Wed Apr 10 2024",
+    "weight": 321.6
+  },
+  {
+    "date": "Thu Apr 11 2024",
+    "weight": 321.4
+  },
+  {
+    "date": "Fri Apr 12 2024",
+    "weight": 319.8
+  },
+  {
+    "date": "Sat Apr 13 2024",
+    "weight": 324.2
+  },
+  {
+    "date": "Sun Apr 14 2024",
+    "weight": 321.0
+  },
+  {
+    "date": "Mon Apr 15 2024",
+    "weight": 321.8
+  },
+  {
+    "date": "Tue Apr 16 2024",
+    "weight": 321.4
+  },
+  {
+    "date": "Wed Apr 17 2024",
+    "weight": 321.6
+  },
+  {
+    "date": "Thu Apr 18 2024",
+    "weight": 321.6
+  }
+]
+
+export const debugWorkoutSchedule =
+[
+  // Sunday
+  {
+    "type": "abs/cardio",
+    "exercises":
+    [
+      {
+        "id": 1,
+        "exerciseName": "dynamic stretches",
+        "sets": null,
+        "reps": null,
+        "duration": null
+      },
+      {
+        "id": 2,
+        "exerciseName": "hanging curls",
+        "sets": 3,
+        "reps": 10,
+        "duration": null
+      },
+      {
+        "id": 3,
+        "exerciseName": "hanging twists",
+        "sets": 3,
+        "reps": 10,
+        "duration": null
+      },
+      {
+        "id": 4,
+        "exerciseName": "decline sit-ups",
+        "sets": 3,
+        "reps": 10,
+        "duration": null
+      },
+      {
+        "id": 5,
+        "exerciseName": "treadmill",
+        "sets": null,
+        "reps": null,
+        "duration": "30 min"
+      }
+    ]
+  },
+  // Monday
+  {
+    "type": "push/cardio",
+    "exercises":
+    [
+      {
+        "id": 1,
+        "exerciseName": "dynamic stretches",
+        "sets": null,
+        "reps": null,
+        "duration": null
+      },
+      {
+        "id": 2,
+        "exerciseName": "bench press",
+        "sets": 5,
+        "reps": 5,
+        "duration": null
+      },
+      {
+        "id": 3,
+        "exerciseName": "incline shoulder press",
+        "sets": 3,
+        "reps": 10,
+        "duration": null
+      },
+      {
+        "id": 4,
+        "exerciseName": "shoulder press",
+        "sets": 3,
+        "reps": 8,
+        "duration": null
+      },
+      {
+        "id": 5,
+        "exerciseName": "lateral raise",
+        "sets": 5,
+        "reps": 12,
+        "duration": null
+      },
+      {
+        "id": 6,
+        "exerciseName": "pec fly",
+        "sets": 3,
+        "reps": 10,
+        "duration": null
+      },
+      {
+        "id": 7,
+        "exerciseName": "cycling",
+        "sets": null,
+        "reps": null,
+        "duration": "30 min"
+      }
+    ]
+  },
+  // Tuesday
+  {
+    "type": "pull/abs",
+    "exercises":
+    [
+      {
+        "id": 1,
+        "exerciseName": "dynamic stretches",
+        "sets": null,
+        "reps": null,
+        "duration": null
+      },
+      {
+        "id": 2,
+        "exerciseName": "pull ups",
+        "sets": 3,
+        "reps": 8,
+        "duration": null
+      },
+      {
+        "id": 3,
+        "exerciseName": "cable rows",
+        "sets": 2,
+        "reps": 10,
+        "duration": null
+      },
+      {
+        "id": 4,
+        "exerciseName": "single hand rows",
+        "sets": 2,
+        "reps": 8,
+        "duration": null
+      },
+      {
+        "id": 5,
+        "exerciseName": "lat pull downs (hammer grip)",
+        "sets": 3,
+        "reps": 10,
+        "duration": null
+      },
+      {
+        "id": 6,
+        "exerciseName": "lat prayers",
+        "sets": 3,
+        "reps": 10,
+        "duration": null
+      },
+      {
+        "id": 7,
+        "exerciseName": "rear delt fly",
+        "sets": 4,
+        "reps": 10,
+        "duration": null
+      },
+      {
+        "id": 8,
+        "exerciseName": "hanging curls",
+        "sets": 3,
+        "reps": 10,
+        "duration": null
+      }
+    ]
+  },
+  // Wednesday
+  {
+    "type": "legs/cardio",
+    "exercises":
+    [
+      {
+        "id": 1,
+        "exerciseName": "dynamic stretches",
+        "sets": null,
+        "reps": null,
+        "duration": null
+      },
+      {
+        "id": 2,
+        "exerciseName": "back squat",
+        "sets": 5,
+        "reps": 5,
+        "duration": null
+      },
+      {
+        "id": 3,
+        "exerciseName": "leg press (low feet placement)",
+        "sets": 3,
+        "reps": 8,
+        "duration": null
+      },
+      {
+        "id": 4,
+        "exerciseName": "quad extension",
+        "sets": 3,
+        "reps": 10,
+        "duration": null
+      },
+      {
+        "id": 5,
+        "exerciseName": "bulgarian split squats",
+        "sets": 3,
+        "reps": 10,
+        "duration": null
+      },
+      {
+        "id": 6,
+        "exerciseName": "goblet squats",
+        "sets": 3,
+        "reps": 8,
+        "duration": null
+      },
+      {
+        "id": 7,
+        "exerciseName": "calf raises",
+        "sets": 3,
+        "reps": 10,
+        "duration": null
+      },
+      {
+        "id": 8,
+        "exerciseName": "treadmill",
+        "sets": null,
+        "reps": null,
+        "duration": "30 min"
+      }
+    ]
+  },
+  // Thursday
+  {
+    "type": "push/abs",
+    "exercises":
+    [
+      {
+        "id": 1,
+        "exerciseName": "dynamic stretches",
+        "sets": null,
+        "reps": null,
+        "duration": null
+      },
+      {
+        "id": 2,
+        "exerciseName": "incline bench press",
+        "sets": 5,
+        "reps": 5,
+        "duration": null
+      },
+      {
+        "id": 3,
+        "exerciseName": "shoulder press",
+        "sets": 3,
+        "reps": 8,
+        "duration": null
+      },
+      {
+        "id": 4,
+        "exerciseName": "incline dumbbell press",
+        "sets": 3,
+        "reps": 10,
+        "duration": null
+      },
+      {
+        "id": 5,
+        "exerciseName": "lateral raise",
+        "sets": 5,
+        "reps": 12,
+        "duration": null
+      },
+      {
+        "id": 6,
+        "exerciseName": "dips",
+        "sets": 3,
+        "reps": "failure",
+        "duration": null
+      },
+      {
+        "id": 7,
+        "exerciseName": "tricep pushdown",
+        "sets": 3,
+        "reps": 10,
+        "duration": null
+      },
+      {
+        "id": 8,
+        "exerciseName": "hanging twists",
+        "sets": 3,
+        "reps": 10,
+        "duration": null
+      }
+    ]
+  },
+  // Friday
+  {
+    "type": "pull/cardio",
+    "exercises":
+    [
+      {
+        "id": 1,
+        "exerciseName": "dynamic stretches",
+        "sets": null,
+        "reps": null,
+        "duration": null
+      },
+      {
+        "id": 2,
+        "exerciseName": "lat pull downs (curl grip)",
+        "sets": 3,
+        "reps": 8,
+        "duration": null
+      },
+      {
+        "id": 3,
+        "exerciseName": "bent over rows",
+        "sets": 3,
+        "reps": 8,
+        "duration": null
+      },
+      {
+        "id": 4,
+        "exerciseName": "cable rows",
+        "sets": 3,
+        "reps": 10,
+        "duration": null
+      },
+      {
+        "id": 5,
+        "exerciseName": "lat pull downs (hammer grip)",
+        "sets": 3,
+        "reps": 10,
+        "duration": null
+      },
+      {
+        "id": 6,
+        "exerciseName": "lat prayers",
+        "sets": 3,
+        "reps": 10,
+        "duration": null
+      },
+      {
+        "id": 7,
+        "exerciseName": "facepulls",
+        "sets": 3,
+        "reps": 10,
+        "duration": null
+      },
+      {
+        "id": 8,
+        "exerciseName": "stairmaster",
+        "sets": null,
+        "reps": null,
+        "duration": "30 min"
+      }
+    ]
+  },
+  // Saturday
+  {
+    "type": "legs/abs",
+    "exercises":
+    [
+      {
+        "id": 1,
+        "exerciseName": "dynamic stretches",
+        "sets": null,
+        "reps": null,
+        "duration": null
+      },
+      {
+        "id": 2,
+        "exerciseName": "back squat",
+        "sets": 5,
+        "reps": 5,
+        "duration": null
+      },
+      {
+        "id": 3,
+        "exerciseName": "leg press (high feet placement)",
+        "sets": 3,
+        "reps": 8,
+        "duration": null
+      },
+      {
+        "id": 4,
+        "exerciseName": "RDLs",
+        "sets": 3,
+        "reps": 8,
+        "duration": null
+      },
+      {
+        "id": 5,
+        "exerciseName": "hamstring curls",
+        "sets": 3,
+        "reps": 10,
+        "duration": null
+      },
+      {
+        "id": 6,
+        "exerciseName": "hip thrusts",
+        "sets": 3,
+        "reps": 10,
+        "duration": null
+      },
+      {
+        "id": 7,
+        "exerciseName": "calf raises",
+        "sets": 3,
+        "reps": 10,
+        "duration": null
+      },
+      {
+        "id": 8,
+        "exerciseName": "decline sit-ups",
+        "sets": 3,
+        "reps": 10,
+        "duration": null
+      }
+    ]
+  }
+]
+
+export const debugMealPlan =
+{
+  "dietType": "weight loss",
+  "meals":
+  [
+    // Sunday
+    {
+      "breakfast":
+      {
+        "mealName": "veggie ham and cheese breakfast casserole",
+        "ingredients": ["eggs", "tomatoes"]
+      },
+      "lunch":
+      {
+        "mealName": "slow cooker 3-bean turkey chili",
+        "ingredients": ["eggs", "tomatoes"]
+      },
+      "dinner":
+      {
+        "mealName": "baked chicken thighs with brussels and sweet potato",
+        "ingredients": ["eggs", "tomatoes"]
+      }
+    },
+    // Monday
+    {
+      "breakfast":
+      {
+        "mealName": "freezer breakfast burritos",
+        "ingredients": ["eggs", "tomatoes"]
+      },
+      "lunch":
+      {
+        "mealName": "black-eyed pea dip w/ perfect air fryer shrimp",
+        "ingredients": ["eggs", "tomatoes"]
+      },
+      "dinner":
+      {
+        "mealName": "huevos rancheros w/ 1 oz avocado",
+        "ingredients": ["eggs", "tomatoes"]
+      }
+    },
+    // Tuesday
+    {
+      "breakfast":
+      {
+        "mealName": "freezer breakfast burritos",
+        "ingredients": ["eggs", "tomatoes"]
+      },
+      "lunch":
+      {
+        "mealName": "kale salad w/ chicken",
+        "ingredients": ["eggs", "tomatoes"]
+      },
+      "dinner":
+      {
+        "mealName": "ground turkey taco skillet",
+        "ingredients": ["eggs", "tomatoes"]
+      }
+    },
+    // Wednesday
+    {
+      "breakfast":
+      {
+        "mealName": "freezer breakfast burritos",
+        "ingredients": ["eggs", "tomatoes"]
+      },
+      "lunch":
+      {
+        "mealName": "kale salad w/ chicken",
+        "ingredients": ["eggs", "tomatoes"]
+      },
+      "dinner":
+      {
+        "mealName": "butternut squash lasagna w/ green salad",
+        "ingredients": ["eggs", "tomatoes"]
+      }
+    },
+    // Thursday
+    {
+      "breakfast":
+      {
+        "mealName": "air fryer breakfast banana split",
+        "ingredients": ["eggs", "tomatoes"]
+      },
+      "lunch":
+      {
+        "mealName": "kale salad w/ chicken",
+        "ingredients": ["eggs", "tomatoes"]
+      },
+      "dinner":
+      {
+        "mealName": "(leftover) butternut squash lasagna w/ green salad",
+        "ingredients": ["eggs", "tomatoes"]
+      }
+    },
+    // Friday
+    {
+      "breakfast":
+      {
+        "mealName": "air fryer breakfast banana split",
+        "ingredients": ["eggs", "tomatoes"]
+      },
+      "lunch":
+      {
+        "mealName": "kale salad w/ chicken",
+        "ingredients": ["eggs", "tomatoes"]
+      },
+      "dinner":
+      {
+        "mealName": "honey sriracha roasted salmon rice bowls",
+        "ingredients": ["eggs", "tomatoes"]
+      }
+    },
+    // Saturday
+    {
+      "breakfast":
+      {
+        "mealName": "veggie ham and cheese breakfast casserole",
+        "ingredients": ["eggs", "tomatoes"]
+      },
+      "lunch":
+      {
+        "mealName": "slow cooker 3-bean turkey chili",
+        "ingredients": ["eggs", "tomatoes"]
+      },
+      "dinner":
+      {
+        "mealName": "baked chicken thighs with brussels and sweet potato",
+        "ingredients": ["eggs", "tomatoes"]
+      }
+    }
+  ]
+}
+
+export const debugPersonalBests =
+{
+  "one mile run":
+  {
+    "date": "Thu Apr 18 2024",
+    "personalBest": "06:54.32"
+  },
+  "bench press":
+  {
+    "date": "Thu Apr 18 2024",
+    "personalBest": 225
+  },
+  "back squat":
+  {
+    "date": "Thu Apr 18 2024",
+    "personalBest": 315
+  },
+  "deadlift":
+  {
+    "date": "Thu Apr 18 2024",
+    "personalBest": 315
+  }
+}
+
+export const debugAchievements =
+{
+  "one mile run":
+  {
+    "First Steps":
+    {
+      "unlocked": true,
+      "description": "Run a mile in under 20 minutes",
+      "date": "Thu Apr 18 2024"
+    },
+    "Making My Way Downtown":
+    {
+      "unlocked": true,
+      "description": "Run a mile in under 15 minutes",
+      "date": "Thu Apr 18 2024"
+    },
+    "Can't Stop, Won't Stop":
+    {
+      "unlocked": true,
+      "description": "Run a mile in under 12 minutes",
+      "date": "Thu Apr 18 2024"
+    },
+    "One Must Walk...":
+    {
+      "unlocked": true,
+      "description": "Run a mile in under 11 minutes",
+      "date": "Thu Apr 18 2024"
+    },
+    "...Before They Run!":
+    {
+      "unlocked": true,
+      "description": "Run a mile in under 10 minutes",
+      "date": "Thu Apr 18 2024"
+    },
+    "Gingerbread Man":
+    {
+      "unlocked": true,
+      "description": "Run a mile in under 9 minutes",
+      "date": "Thu Apr 18 2024"
+    },
+    "One Must Run...":
+    {
+      "unlocked": true,
+      "description": "Run a mile in under 8 minutes",
+      "date": "Thu Apr 18 2024"
+    },
+    "...Before They Fly!":
+    {
+      "unlocked": true,
+      "description": "Run a mile in under 7 minutes",
+      "date": "Thu Apr 18 2024"
+    },
+    "Unstoppable":
+    {
+      "unlocked": false,
+      "description": "Run a mile in under 6 minutes",
+      "date": null
+    },
+    "The Flash":
+    {
+      "unlocked": false,
+      "description": "Run a mile in under 5 minutes",
+      "date": null
+    },
+  },
+  "bench press":
+  {
+    "One Star Bench":
+    {
+      "unlocked": true,
+      "description": "Bench Press 1 Plate (135+ lbs.)",
+      "date": "Thu Apr 18 2024"
+    },
+    "Two Star Bench":
+    {
+      "unlocked": true,
+      "description": "Bench Press 2 Plates (225+ lbs.)",
+      "date": "Thu Apr 18 2024"
+    },
+    "Three Star Bench":
+    {
+      "unlocked": false,
+      "description": "Bench Press 3 Plates (315+ lbs.)",
+      "date": null
+    },
+    "Bench Beast":
+    {
+      "unlocked": false,
+      "description": "Bench Press 4 Plates (405+ lbs.)",
+      "date": null
+    }
+  },
+  "back squat":
+  {
+    "One Star Squat":
+    {
+      "unlocked": true,
+      "description": "Back Squat 1 Plate (135+ lbs.)",
+      "date": "Thu Apr 18 2024"
+    },
+    "Two Star Squat":
+    {
+      "unlocked": true,
+      "description": "Back Squat 2 Plates (225+ lbs.)",
+      "date": "Thu Apr 18 2024"
+    },
+    "Three Star Squat":
+    {
+      "unlocked": true,
+      "description": "Back Squat 3 Plates (315+ lbs.)",
+      "date": "Thu Apr 18 2024"
+    },
+    "Sasquat":
+    {
+      "unlocked": false,
+      "description": "Back Squat 4 Plates (405+ lbs.)",
+      "date": null
+    }
+  },
+  "deadlift":
+  {
+    "One Star Deadlift":
+    {
+      "unlocked": true,
+      "description": "Deadlift 1 Plate (135+ lbs.)",
+      "date": "Thu Apr 18 2024"
+    },
+    "Two Star Deadlift":
+    {
+      "unlocked": true,
+      "description": "Deadlift 2 Plates (225+ lbs.)",
+      "date": "Thu Apr 18 2024"
+    },
+    "Three Star Deadlift":
+    {
+      "unlocked": true,
+      "description": "Deadlift 3 Plates (315+ lbs.)",
+      "date": "Thu Apr 18 2024"
+    },
+    "Deadly Lift":
+    {
+      "unlocked": true,
+      "description": "Deadlift 4 Plates (405+ lbs.)",
+      "date": "Thu Apr 18 2024"
+    },
+    "Deadlift Devil":
+    {
+      "unlocked": false,
+      "description": "Deadlift 5 Plates (495+ lbs.)",
+      "date": null
+    },
+  },
+}
--- Fitnesse-Beta/screens/HomeScreen.js
+++ Fitnesse-Beta/screens/HomeScreen.js
@@ -1,6 +1,9 @@
 import { View, Text, StyleSheet, Pressable } from "react-native";
 import { useState } from "react";
 
+import { debugHeight, debugWeightHistory, debugWorkoutSchedule, debugMealPlan,
+        debugPersonalBests, debugAchievements } from "../data/debugDataSets";
+
 export default function HomeScreen({ navigation }) {
   function debugStuff() {
     let dateToday = new Date().toDateString();
@@ -9,38 +12,9 @@ export default function HomeScreen({ navigation }) {
     console.log(dateYesterday);
   }
 
-  function todoCards() {
-    // check if user hasn't calculated Weight Goal (Async)
-    let userWeightHistory = null;
-    let userHeight = 1;
-    if (userWeightHistory == null || userHeight == null) {
-      // return conditionally rendered elements - can change state after finishing task
-      return (
-        <Pressable onPress={() => navigation.navigate('Profile Stack', { screen: 'Weight Goal' })}>
-          <Text style={styles.testText}>Calculate Your Weight Goal</Text>
-        </Pressable>
-      );
-    }
-
-    // check if user hasn't recorded Today's Weight (Async)
-    let dateToday = new Date().toDateString();
-    let latestDayRecorded = new Date(2024, 3, 18).toDateString();
-    if (dateToday != latestDayRecorded) {
-      return (
-        <Pressable onPress={() => navigation.navigate('Profile Stack', { screen: 'Record Weight' })}>
-          <Text style={styles.testText}>Record Today's Weight</Text>
-        </Pressable>
-      );
-    }
-
-    // otherwise, nothing else to do
-    return <Text style={styles.testText}>All Done!</Text>;
-  }
-
   return (
     <View style={styles.container}>
       <Text style={styles.text}>To-Do:</Text>
-      {todoCards()}
       <Pressable onPress={debugStuff}>
         <Text style={styles.debug}>Scuffed Debug Button</Text>
       </Pressable>
